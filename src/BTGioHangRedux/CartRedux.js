e70cf59724a23afa2bba7eeddbbc61e53f28bca0
Nguyen <tranchauthanhnguyen@gmail.com>
2024-04-26 14:26:52 +0700

    buoi8

--- src/BTGioHangRedux/CartRedux.js
+++ src/BTGioHangRedux/CartRedux.js
@@ -1,9 +1,155 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-export default class CartRedux extends Component {
+//* B3: kết nối component với store
+class CartRedux extends Component {
+
+  renderCart = () => {
+    let { gioHang } = this.props
+    return gioHang.map((cartItem) => {
+      let { maSP, hinhAnh, giaBan, soLuong, tenSP } = cartItem
+      return <tr key={`cart${maSP}`}>
+        <td>{maSP}</td>
+        <td>
+          <img style={{ width: "50px" }} src={hinhAnh} alt="" />
+        </td>
+        <td>{tenSP}</td>
+
+        <td>
+          <button onClick={() => {
+            
+            let action = {
+              type:"TANG_GIAM",
+              maSPClick: maSP,
+              sl: -1
+            }
+            this.props.dispatch(action)
+
+          }} className='btn btn-success'>-</button>
+          <span> {soLuong} </span>
+          <button onClick={() => {
+            let action = {
+              type: "TANG_GIAM",
+              maSPClick: maSP,
+              sl: 1
+            }
+            this.props.dispatch(action)
+
+          }} className='btn btn-success'>+</button>
+        </td>
+
+        <td>{giaBan.toLocaleString()}</td>
+        <td>{(giaBan * soLuong).toLocaleString()}</td>
+        <td>
+          <button onClick={() => {
+            
+            const action = {
+              type:"XOA_GIO_HANG",
+              maSPXoa: maSP
+            }
+
+            this.props.dispatch(action)
+
+          }}
+
+            className='btn btn-danger'>Xóa</button>
+        </td>
+      </tr>
+    })
+  }
+
+  sumPrice = () => {
+
+    return this.props.gioHang.reduce((total, cartItem, index) => total += cartItem.soLuong * cartItem.giaBan, 0).toLocaleString()
+
+  }
+
+
+
+
+
   render() {
+    console.log(this.props)
     return (
-      <div>CartRedux</div>
+      <div className="modal fade" id="exampleModal" tabIndex={-1} aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog modal-lg">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+              <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">×</span>
+              </button>
+            </div>
+            <div className="modal-body">
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th scope="col">Mã sản phẩm</th>
+                    <th scope="col">Hình ảnh</th>
+                    <th scope="col">Tên sản phẩm</th>
+                    <th scope="col">Số lượng</th>
+                    <th scope="col">Đơn giá</th>
+                    <th scope="col">Thành tiền</th>
+                    <th scope="col"></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {this.renderCart()}
+                </tbody>
+              </table>
+
+              {/* <div>Tổng số tiền: {this.props.sumPrice().toLocaleString()} </div> */}
+              <div>Tổng số tiền:
+                {this.sumPrice()} 
+              </div>
+
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button type="button" className="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+        </div>
+      </div>
     )
   }
 }
+
+const mapStateToProp = (rootReducer) => {
+  // tra về state gioHang vào prop của component
+  //this.props.gioHang
+  return {
+    gioHang: rootReducer.gioHangReducer
+  }
+}
+
+//component mới (react component + Redux)
+const ComponentCartRedux = connect(mapStateToProp)(CartRedux)
+
+export default ComponentCartRedux; //export để react component cha sử dụng component redux 
+
+
+
+
+
+//*Callback function
+// gọi hàm 
+//showInfo có return
+// main(showInfo)
+
+//*Closure functions
+// connect()()
+// Khai báo
+// function main() {
+//   function showInfo() {
+//     console.log("123")
+//   }
+// }
+
+// gọi hàm
+// main() => gọi hàm main
+// main()() //=> hàm Closure function
+
+
+
+
--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -2,9 +2,82 @@
 // createStore: sử dụng cho class component
 import { combineReducers, createStore } from "redux";
 
+//xác định kiểu dữ liệu của state
+//dữ liệu nào cần lưu vào state
+
+//*B1: khai báo giá trị ban đầu cho state
+const gioHang = []
+
 const rootReducer = combineReducers({
     //reducers: các state sẽ được lưu ở đây
+    //*B2: Tạo reducer, phương thức chứa state (nhận vào tham số state) 
+    gioHangReducer: (state = gioHang, action) => {
+        console.log(action)
+        switch (action.type) {
+            case "THEM_GIO_HANG":
+                //* tạo đối tượng riêng cho giỏ hàng
+                let { maSP, tenSP, giaBan, hinhAnh } = action.phone;
+                let cartItem = { maSP: maSP, tenSP: tenSP, giaBan: giaBan, hinhAnh: hinhAnh, soLuong: 1 }
+
+                //copy mảng sang mảng mơi
+                let gioHangUpdate = [...state]
+                let cartFind = gioHangUpdate.find(cartItem => cartItem.maSP === maSP)
+                if (cartFind) {
+                    cartFind.soLuong += 1;
+                } else {
+
+                    gioHangUpdate.push(cartItem);
+                }
+                // return state;// giỏ hàng ban đầu
+                // tính bất biến => tham chiếu
+                // => phải trả về biến mới => địa chỉ ô nhớ => redux mới nhận biết thay đổi => render lại UI
+                return gioHangUpdate; //trả về giỏ hàng mới
+
+            case "XOA_GIO_HANG":
+
+                let gioHangMoi = state.filter(cartItem => cartItem.maSP !== action.maSPXoa)
+
+                return gioHangMoi
+            case "TANG_GIAM":
+                let gioHangUpdateSL = [...state]
+
+                let findItem = gioHangUpdateSL.find((cartItem) => {
+                    return cartItem.maSP === action.maSPClick
+                })
+
+                if (findItem) {
+                    //tìm thấy
+                    findItem.soLuong += action.sl
+                    if (findItem.soLuong < 1) {
+                        alert("Số lượng không được dưới 1")
+                        findItem.soLuong -= action.sl
+                    }
+                }
+
+                return gioHangUpdateSL
+
+            default:
+                return state;
+        }
+
+        // trả về state
+        // return state;// gioHang
+    },
+
+    //mỗi reducer giống 1 tính năng của ứng dụng
+    // xemChiTietReducer: ()=>{
+    // }
 
 })
 
 export const store = createStore(rootReducer)
+
+
+// class MyClass{
+//     first = () => { 
+//      }
+// }
+// const rootReducer = {
+//     gioHangReducer: function(){
+//     }
+// }
